4105434fcfda0129b7145067ab5f8018f6fedf82
Satyam <satyamjha4@gmail.com>
2025-06-14 15:41:49 +0530

    server side completed

--- Backend/src/router/notesRoutes.js
+++ Backend/src/router/notesRoutes.js
@@ -7,9 +7,7 @@ const router = express.Router();
 router.get('/', getallNotes);
 router.get('/:id', getNotebyID);
 router.post('/', createNote);
-
 router.put('/:id', updateNote);
-
 router.delete('/:id', deleteNote);
 
 export default router;
--- Backend/src/server.js
+++ Backend/src/server.js
@@ -2,6 +2,7 @@ import express from 'express'
 import notesRoutes from './router/notesRoutes.js'
 import { connectDB } from './config/db.js';
 import dotenv from 'dotenv'
+import ratelimiter from './middleware/rateLimter.js';
 
 dotenv.config()
 
@@ -11,14 +12,14 @@ const PORT = process.env.PORT;
 
 // middleware
 app.use(express.json())
+app.use(ratelimiter)
 
 app.use('/api/notes', notesRoutes)
 
-connectDB(process.env.DATABASE_URL)
+connectDB(process.env.DATABASE_URL).then(() => {
+    app.listen(PORT, () => {
+        console.log(`Server is running on port ${PORT}`);
+    });
+})
 
 
-
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-    }
-);
