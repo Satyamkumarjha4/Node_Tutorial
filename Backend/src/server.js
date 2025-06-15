7dd25107d4fd1257385287582106bfee219660e8
Satyam <satyamjha4@gmail.com>
2025-06-15 14:34:33 +0530

    ho gya aakhir kuch to

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -1,28 +1,32 @@
-import express from 'express'
-import cors from 'cors'
-import dotenv from 'dotenv'
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import path from "path";
 
-import notesRoutes from './router/notesRoutes.js'
-import { connectDB } from './config/db.js';
-import ratelimiter from './middleware/rateLimter.js';
+import notesRoutes from "./router/notesRoutes.js";
+import { connectDB } from "./config/db.js";
+import rateLimiter from "./middleware/rateLimter.js";
 
-dotenv.config()
+dotenv.config();
 
 const app = express();
-
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 5001;
+const __dirname = path.resolve();
 
 // middleware
-app.use(express.json())
-app.use(cors())
-app.use(ratelimiter)
 
-app.use('/api/notes', notesRoutes)
+app.use(
+  cors()
+);
+
+app.use(express.json()); // this middleware will parse JSON bodies: req.body
+app.use(rateLimiter);
+
+app.use("/api/notes", notesRoutes);
+
 
 connectDB(process.env.DATABASE_URL).then(() => {
     app.listen(PORT, () => {
-        console.log(`Server is running on port ${PORT}`);
-    });
-})
-
-
+        console.log("Server started on PORT:", PORT);
+      }); 
+});
